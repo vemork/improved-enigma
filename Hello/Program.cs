366212af72c8b5db629ac576d9328cc27c5c8996
Doyler Emmanuel Rubio Villamizar <doyler.rubio@mercadolibre.com.co>
2025-01-11 21:35:53 -0500

    adding contents

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// variable declaration local Camel Case
+int number;
+int NumberImportant = 1;
+
+// variable declaration constant Pascal Case
+const float Pi = 3.14f;
+
+// error naming convention
+//int 1route;
+
+float number6 = 1.23f;
+double number1 = 1.23;
+decimal number2 = 1.23m;
+
+byte number3;
+//console.writeline(number3); // error not initialized
+
+System.Console.WriteLine(1 + 2); // 3
+System.Console.WriteLine(number);
+
+// Interpolación de cadenas
+
+string name = "Alice";
+int age = 30;
+
+string message = $"Hola, mi nombre es {name} y tengo {age} años.";
+
+Console.WriteLine(message);
+
